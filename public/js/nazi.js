90129ede6ae7c85c9f542afaa96041b17daf134d
mario-sanmartin <sanmartin.mario@outlook.com>
2020-12-22 00:47:48 -0300

    reorganizando

--- public/js/nazi.js
+++
@@ -1,5 +0,0 @@
-const bg = document.getElementById('header');
-window.addEventListener('scroll',()=>{
-    bg.style.backgroundSize = 160- +window.pageYOffset/12+'%';
-    bg.style.opacity = 1- +window.pageYOffset/700+'';
-})
